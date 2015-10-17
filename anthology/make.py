8d5efa3daa7b2d4dd13a2d9686253bc2dc097795
islab <islab@ismail.csie.ncku.edu.tw>
2015-10-18 00:51:47 +0800

    new file arch

---
+++ anthology/make.py
@@ -0,0 +1,21 @@
+import sys
+import urllib2
+
+def load_all_content (dest) :
+	try :
+		headers = {
+			'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'
+		}
+		req = urllib2.Request(
+			url = dest,
+			headers = headers
+		)
+		return urllib2.urlopen(req).read()
+	except:
+		return None
+
+
+templetefile = open('templete.html', 'r')
+templetestring = templetefile.read();
+
+templetestring.replace('', '');
