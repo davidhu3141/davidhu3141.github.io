dfabb53697f78c237f32949b327cb2cc64376bc2
ChiaWei <davidhu8888@livemail.tw>
2015-10-23 05:21:32 +0800

    python travrse and combine

--- anthology/make.py
+++ anthology/make.py
@@ -1,26 +1,45 @@
 import sys
 import json
+from os import walk
+from os import makedirs
+from os import path
+import bytify
 
 # templete file
 templeteFile = open('templete.html', 'r')
 templeteString = templeteFile.read()
 
 # json file
-jsonArticleListFile = open('generate_conf.json', 'r')
-jsonArticleListString = jsonArticleListFile.read()
-jsonArticleList = json.loads(jsonArticleListString)
-jsonArticleList = byteify(jsonArticleList)
+confJsonFile = open('conf.json', 'r')
+confJsonString = confJsonFile.read()
+confJson = json.loads(confJsonString)
+confJson = bytify.byteify(confJson)
 
-articles = jsonArticleList['articles']
-initialPath = jsonArticleList['initialpath']
-saveFolder = 'tempHTML/'
+fromFolder = 'stackeditHTML/'
+saveFolder = 'generatedHTML/'
 
-for o in articles:
-	particularPath = o['path']
-	saveFilename = particularPath
-	articleBody = loadAllContent(initialPath + particularPath)
-	MixedString = templeteString.replace('<!--@Article-->', articleBody).replace('<!--@RootPath-->', 'https://davidhu3141.github.io');
+for (dirpath, dirnames, filenames) in walk(fromFolder):
+	for filename in filenames:
+		if filename.endswith('.html'): 
 
-	# save file
-	outputfile = open(saveFolder + saveFilename, 'w+');
-	outputfile.write(MixedString);
+			loadPath = ''.join(dirpath) + '/' + filename
+			savePath = loadPath.replace(fromFolder, saveFolder)
+
+			articleBody = open(loadPath, 'r').read()
+			MixedString = templeteString.replace('<!--@Article-->', articleBody)
+
+			if not path.exists(path.dirname(savePath)):
+				makedirs(path.dirname(savePath))
+
+			outputfile = open(savePath, 'w+');
+			outputfile.write(MixedString);
+
+#for o in articles:
+#	particularPath = o['path']
+#	saveFilename = particularPath
+#	articleBody = loadAllContent(initialPath + particularPath)
+#	MixedString = templeteString.replace('<!--@Article-->', articleBody).replace('<!--@RootPath-->', 'https://davidhu3141.github.io');
+#
+#	# save file
+#	outputfile = open(saveFolder + saveFilename, 'w+');
+#	outputfile.write(MixedString);
