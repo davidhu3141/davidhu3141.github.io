c213b7c92ff41f395a86db286f078ef5d52eb0a7
islab <islab@ismail.csie.ncku.edu.tw>
2015-10-23 20:48:35 +0800

    articleList added

--- anthology/make.py
+++ anthology/make.py
@@ -1,24 +1,56 @@
 import sys
+sys.path.append('./shell')
+
 import json
 from os import walk
 from os import makedirs
 from os import path
 import bytify
 
-# templete file
-templeteFile = open('templete.html', 'r')
-templeteString = templeteFile.read()
-
 # json file
-confJsonFile = open('conf.json', 'r')
+confJsonFile = open('templete/conf.json', 'r')
 confJsonString = confJsonFile.read()
 confJson = json.loads(confJsonString)
 confJson = bytify.byteify(confJson)
 
-webResourceRoot = 'https://davidhu3141.github.io/anthology'
-fromFolder = 'stackeditHTML'
-saveFolder = 'generatedHTML'
+# templete file
+templeteFile = open(confJson['templetePath'], 'r')
+templeteString = templeteFile.read()
 
+# other generate settings
+webResourceRoot = confJson['webResourceRoot']
+fromFolder = confJson['sourcePath']
+saveFolder = confJson['destinationPath']
+
+# article list pregenerate
+categoryStruct = {}
+for (dirpath, dirnames, filenames) in walk(fromFolder):
+	for filename in filenames:
+		if filename.endswith('.html'):
+			
+			categoryName = ''.join(dirpath).replace(confJson['sourcePath'] + '/', '')
+			titleName = filename.replace('.html', '')
+			
+			if categoryName not in categoryStruct:
+				categoryStruct[categoryName] = []
+			
+			categoryStruct[categoryName].extend([titleName])
+
+# article list generate
+articleListString = ''
+for c in categoryStruct:
+	articleListString += '<div class="category">\n'
+	articleListString += '<div class="categoryTitle">' + c + '</div>\n'
+
+	articleListString += '<div class="categoryList">\n'
+	for f in categoryStruct[c]:
+		articleListString += '<div class="articleLink"><a href="' + confJson['webResourceRoot'] + confJson['destinationPath'] + '/' + c + '/' + f + '.html' + '">' + f + '</a></div>\n'
+	articleListString += '</div>\n'
+
+	articleListString += '</div>\n'
+
+print articleListString
+		
 for (dirpath, dirnames, filenames) in walk(fromFolder):
 	for filename in filenames:
 		if filename.endswith('.html'): 
@@ -26,9 +58,11 @@ for (dirpath, dirnames, filenames) in walk(fromFolder):
 			loadPath = ''.join(dirpath) + '/' + filename
 			savePath = loadPath.replace(fromFolder, saveFolder)
 
-			articleBody = open(loadPath, 'r').read()
-			MixedString = templeteString.replace('<!--@Article-->', articleBody)
-			MixedString =    MixedString.replace('<!--@WebResourceRoot-->', webResourceRoot)
+			articleBodyString = open(loadPath, 'r').read()
+			MixedString = templeteString
+			MixedString = MixedString.replace('<!--@WebResourceRoot-->', webResourceRoot)
+			MixedString = MixedString.replace('<!--@Article-->', articleBodyString)
+			MixedString = MixedString.replace('<!--@ArticleList-->', articleListString)
 
 			if not path.exists(path.dirname(savePath)):
 				makedirs(path.dirname(savePath))
