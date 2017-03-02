c5c3f1724851d295fd4a8d61a6d909e6b56c253e
autodavid <davidhu8888@livemail.tw>
2017-03-03 01:44:40 +0800

    add article list and remove unused productions

--- _site/NormalFiles/anthology/module/walktest.py
+++
@@ -1,7 +0,0 @@
-from os import *
-
-for (dirpath, dirnames, filenames) in walk("./"):
-	for filename in filenames:
-		if filename.endswith('.html'): 
-			fullpath = (''.join(dirpath) + '/' + filename).replace('./', '')
-			print '<a href="' + fullpath + '">' + filename + "</a>"
