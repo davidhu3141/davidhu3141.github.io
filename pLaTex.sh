193b356393f3384ef8ee54e03b84dc244150f90f
autodavid <davidhu8888@livemail.tw>
2017-03-06 02:09:37 +0800

    fix LaTeX '_' using whitespace char

--- pLaTex.sh
+++ pLaTex.sh
@@ -1,5 +1,5 @@
 perl -pi -e 's|\$\$(.+?)\$\$|<script type="math\/tex; mode=display">\1</script>|g' _posts/*
-perl -pi -e 's|(^[\#\.\>0-9 \-\[\]]*?)\$(.+?)\$|\1_<script type="math\/tex">\2</script>|u' _posts/*
+perl -pi -e 's|(^[\#\.\>0-9 \-\[\]]*?)\$(.+?)\$|\1 <script type="math\/tex">\2</script>|u' _posts/*
 perl -pi -e 's|\$(.+?)\$|<script type="math\/tex">\1</script>|g' _posts/*
 
 perl -pi -e 's|```|~~~ |g' _posts/*
